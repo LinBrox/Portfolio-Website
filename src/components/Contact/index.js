c1b105d7e7f81e357837b57b25a4c1b87bd404e7
LinBrox <97131958+LinBrox@users.noreply.github.com>
2023-07-31 22:40:39 +0100

    finished contact & emailjs setup

---
+++ src/components/Contact/index.js
@@ -0,0 +1,107 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+  const position = [51.4816, -3.1791]
+
+  useEffect(() => {
+    setLetterClass('text-animate');
+
+    // Add a timeout to change the animation class after 4000ms (4 seconds)
+    const timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+
+    // Clean up the timeout to avoid memory leaks
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm("service_o3kd3kf","template_zqk0rzl", form.current, 'jA616Hp1AOn1qX6un')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="from_name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[51.4816, -3.1791]} zoom={5}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[51.4816, -3.1791]}>
+              <Popup>Cardiff, Wales - Come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+export default Contact
